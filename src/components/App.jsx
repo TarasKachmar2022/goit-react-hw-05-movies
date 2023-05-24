c4b09f83e5d767c63cecc2b0c77d65de962d60fa
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-25 02:23:12 +0300

    add emotion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,30 @@
-import HomePage from 'pages/HomePage';
-import MoviesDetailsPage from 'pages/MoviesDetailsPage';
-import MoviesPage from 'pages/MoviesPage';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout/SharedLayout';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
 import routes from 'routes';
+import Loader from './Loader/Loader';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const MoviesPage = lazy(() => import('pages/MoviesPage'));
+const MoviesDetailsPage = lazy(() => import('pages/MoviesDetailsPage'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
     <>
-      <Routes>
-        <Route path={routes.HOME} element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path={routes.MOVIES} element={<MoviesPage />} />
-          <Route path={routes.MOVIES_DETAILS} element={<MoviesDetailsPage />}>
-            <Route path={routes.MOVIES_CAST} element={<Cast />} />
-            <Route path={routes.MOVIES_REVIEWS} element={<Reviews />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path={routes.HOME} element={<SharedLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path={routes.MOVIES} element={<MoviesPage />} />
+            <Route path={routes.MOVIES_DETAILS} element={<MoviesDetailsPage />}>
+              <Route path={routes.MOVIES_CAST} element={<Cast />} />
+              <Route path={routes.MOVIES_REVIEWS} element={<Reviews />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Global, ThemeProvider } from '@emotion/react';
+import { theme } from 'styled/Theme';
+import './styled/index.css';
+import { globalStyles } from 'styled/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter basename="goit-react-hw-05-movies">
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <Global styles={globalStyles} />
+    </ThemeProvider>
   </BrowserRouter>
   // </React.StrictMode>
 );

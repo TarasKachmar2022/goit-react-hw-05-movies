08e8e90acbce1d4a39a6f105a985a813a612a45a
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-25 12:10:05 +0300

    add not found page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout';
 import routes from 'routes';
 import Loader from './Loader';
+import NotFound from 'pages/NotFound/NotFound';
 
 const HomePage = lazy(() => import('pages/HomePage'));
 const MoviesPage = lazy(() => import('pages/MoviesPage'));
 const MoviesDetailsPage = lazy(() => import('pages/MoviesDetailsPage'));
 const Cast = lazy(() => import('./Cast'));
 const Reviews = lazy(() => import('./Reviews'));
+// const NotFound = lazy(() => import('page/NotFound'));
 
 export const App = () => {
   return (
@@ -22,6 +24,7 @@ export const App = () => {
               <Route path={routes.MOVIES_CAST} element={<Cast />} />
               <Route path={routes.MOVIES_REVIEWS} element={<Reviews />} />
             </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </Suspense>
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import error from '../../images/not-found.webp';
+
+const NotFound = () => {
+  return (
+    <>
+      <img src={error} alt="Not Found 404" />
+    </>
+  );
+};
+
+export default NotFound;

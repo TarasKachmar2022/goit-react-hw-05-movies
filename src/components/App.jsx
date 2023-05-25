d5acf4296853d19af339f67d54cc7d3c42387060
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-25 12:18:11 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './SharedLayout';
 import routes from 'routes';
 import Loader from './Loader';
-import NotFound from 'pages/NotFound/NotFound';
 
 const HomePage = lazy(() => import('pages/HomePage'));
 const MoviesPage = lazy(() => import('pages/MoviesPage'));
 const MoviesDetailsPage = lazy(() => import('pages/MoviesDetailsPage'));
 const Cast = lazy(() => import('./Cast'));
 const Reviews = lazy(() => import('./Reviews'));
-// const NotFound = lazy(() => import('page/NotFound'));
+const NotFound = lazy(() => import('pages/NotFound'));
 
 export const App = () => {
   return (
@@ -24,8 +23,8 @@ export const App = () => {
               <Route path={routes.MOVIES_CAST} element={<Cast />} />
               <Route path={routes.MOVIES_REVIEWS} element={<Reviews />} />
             </Route>
-            <Route path="*" element={<NotFound />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
     </>

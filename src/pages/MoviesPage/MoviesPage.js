925965f2c1e10e67d4ce9111a26b4982bfa93014
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-26 03:11:06 +0300

    add useParams

--- src/pages/MoviesPage/MoviesPage.js
+++ src/pages/MoviesPage/MoviesPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Toaster, toast } from 'react-hot-toast';
 import axios from 'axios';
 import Loader from 'components/Loader';
@@ -8,12 +9,20 @@ import LoadMoreBtn from 'components/LoadMore';
 import MovieList from 'components/MoviesList';
 
 const MoviesPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [searchName, setSearchName] = useState('');
-  const [page, setPage] = useState(1);
+  const [searchName, setSearchName] = useState(
+    () => searchParams.get('query') ?? ''
+  );
+  const [page, setPage] = useState(() => Number(searchParams.get('page')) ?? 1);
   const [pageQuantity, setPageQuantity] = useState(0);
+
+  useEffect(() => {
+    if (!searchName) return;
+    setSearchParams({ query: searchName, page: page });
+  }, [searchName, page, setSearchParams]);
 
   useEffect(() => {
     const FetchSearchMovie = async () => {
